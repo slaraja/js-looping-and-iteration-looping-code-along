4e9505259b5823466a625b3356561aefc3a62851
Stephanie La Raja <stephanielaraja@gmail.com>
2021-07-14 09:19:45 -0400

    commit tree caf66f8bf414f48a5ff89556a93159ef65c436e6

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array) {
+    let newArray = [];
+    for(let i = 0; i < array.length; i++) {
+      newArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return newArray;
+}
+  
+function countDown(number){
+    while(number > -1) {
+        console.log(number--);
+    }
+  
+}
